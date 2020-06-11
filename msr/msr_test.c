2b6dfd67caed4509fa77fabd63a9d93b47e5ee6a
Ning Yang <csyangning@gmail.com>
2020-06-10 23:23:47 -0700

    Add basic rdmsr code

--- msr/msr_test.c
+++ msr/msr_test.c
@@ -6,15 +6,35 @@ MODULE_AUTHOR("Ning Yang");
 MODULE_DESCRIPTION("A simple example Linux module which read/write MSR.");
 MODULE_VERSION("0.01");
 
-static int __init lkm_example_init(void) {
- printk(KERN_INFO "-----------------------------------\n");
- printk(KERN_INFO "|           MSR test              |\n");
- printk(KERN_INFO "-----------------------------------\n");
- return 0;
-}
-static void __exit lkm_example_exit(void) {
- printk(KERN_INFO "Goodbye, World!\n");
+unsigned long long x86_get_msr(int msr)
+{
+    unsigned long msrl = 0, msrh = 0;
+
+    /* NOTE: rdmsr is always return EDX:EAX pair value */
+    asm volatile("rdmsr"
+                 : "=a"(msrl), "=d"(msrh)
+                 : "c"(msr));
+
+    return ((unsigned long long)msrh << 32) | msrl;
 }
 
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+static int __init msr_test_init(void)
+{
+    unsigned long long result;
+    const unsigned int msr = 0x10;
+    printk(KERN_INFO "-----------------------------------\n");
+    printk(KERN_INFO "|           MSR test              |\n");
+    printk(KERN_INFO "-----------------------------------\n");
+
+    result = x86_get_msr(msr);
+    printk(KERN_INFO "Read Msr:\t%#x\n", msr);
+    printk(KERN_INFO "Result:\t%#llx\n", result);
+    return 0;
+}
+static void __exit msr_test_exit(void)
+{
+    printk(KERN_INFO "Unload msr_test.\n");
+}
+
+module_init(msr_test_init);
+module_exit(msr_test_exit);
